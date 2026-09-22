ca9819202e3ae3ebca560dc237eca3143cf29e54
chipm64 <imjam@yandex.ru>
2026-09-22 15:36:20 +0300

    Update Custom.h

--- src/Custom.h
+++ src/Custom.h
@@ -1,21 +1,22 @@
 #ifndef ESPEASY_CUSTOM_H
 #define ESPEASY_CUSTOM_H
 
-// Активируем использование этого файла
+// 1. Активируем кастомную конфигурацию
 #define USE_CUSTOM_H
 
-// Включаем стандартный набор плагинов NORMAL
-#define PLUGIN_SET_NORMAL
+// 2. Полностью отключаем все стандартные наборы плагинов
+#define PLUGIN_SET_NONE
 
-// ПРИНУДИТЕЛЬНО ВКЛЮЧАЕМ НАШ ПЛАГИН SHT3x (P068)
-#define USES_P068
+// 3. ВКЛЮЧАЕМ НАШ ДАТЧИК SHT30 И ДИСПЛЕЙ SSD1306
+#define USES_P068   // Environment - SHT3x (Датчик температуры/влажности)
+#define USES_P023   // Display - OLED SSD1306 (Экран для вывода данных)
 
-// Отключаем самые тяжелые плагины, чтобы код точно влез в 1МБ
-#undef USES_P012   // LCD дисплеи
-#undef USES_P023   // OLED дисплеи
-#undef USES_P036   // Frame OLED
-#undef USES_P028   // Датчик BME280
-#undef USES_P045   // Датчик MPU6050
-#undef USES_P079   // Дисплей Wemos Matrix
+// 4. ДОБАВЛЯЕМ РАСШИРИТЕЛИ ПОРТОВ ДЛЯ УПРАВЛЕНИЯ КЛЮЧАМИ ПО I2C
+#define USES_P019   // Switch input - PCF8574 (8 дополнительных портов)
+#define USES_P009   // Switch input - MCP23017 (16 дополнительных портов)
+
+// 5. ВКЛЮЧАЕМ ТОЛЬКО ОТПРАВКУ НА THINGSPEAK
+#define CONTROLLER_SET_NONE
+#define USES_C004   // ThingSpeak HTTP
 
 #endif // ESPEASY_CUSTOM_H
